186663a6701f53ca91e84ddbe48ca7bb206ee703
Clayton Knapp <csjknapp@gmail.com>
2022-05-16 15:32:48 -0700

    deleted comentted out code

--- src/components/Planner/EntryList.jsx
+++ src/components/Planner/EntryList.jsx
@@ -5,14 +5,7 @@ import { useEntries } from '../../context/PlannerContext';
 export default function Entries() {
   // TODO: Replace with the entries stored in PlannerContext
   const { entries } = useEntries();
-  // const entries = [
-  //   {
-  //     id: 0,
-  //     title: 'Replace me!',
-  //     content: '`content` should only be visible on the detail view',
-  //     date: new Date()
-  //   },
-  // ];
+
 
   return (
     <ul className={styles.list}>
